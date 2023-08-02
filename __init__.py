8081fb3649ffcba0554319a460e061ad519ec61d
Dillon Franke <dillon.franke@protonmail.com>
2023-08-02 13:49:08 -0700

    updated unnessary __init__ functionality

--- __init__.py
+++ __init__.py
@@ -7,23 +7,5 @@ _BASE_DIR = os.path.abspath(
 )
 
 sys.path.insert(0, _BASE_DIR + "/deps/protobuf/python/")
-sys.path.insert(0, _BASE_DIR + "/deps/six/")
 sys.path.insert(0, _BASE_DIR + "/gen")
 
-# Hack to fix loading protobuf libraries within Jython. See https://github.com/protocolbuffers/protobuf/issues/7776
-def fix_protobuf():
-    import six
-
-    u = six.u
-
-    def new_u(s):
-        if s == r"[\ud800-\udfff]":
-            # Don't match anything
-            return "$^"
-        else:
-            return u(s)
-
-    six.u = new_u
-
-
-fix_protobuf()
